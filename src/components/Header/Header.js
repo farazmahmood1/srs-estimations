d4160cf27b88c165631f75d25d926aff85410f4c
Faraz Mahmood <86164728+farazmahmood1@users.noreply.github.com>
2025-04-17 04:25:05 +0500

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './Header.css'; 
+import './Header.css';
 
 const Header = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [showMobileHeader, setShowMobileHeader] = useState(false);
-  const location = useLocation(); // Get the current route
-  const [activeItem, setActiveItem] = useState(location.pathname); // Set the initial active item
+  const location = useLocation();
+  const [activeItem, setActiveItem] = useState(location.pathname);
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
@@ -19,16 +19,13 @@ const Header = () => {
   }, []);
 
   useEffect(() => {
-    setActiveItem(location.pathname); // Update active item on route change
+    setActiveItem(location.pathname);
   }, [location]);
 
   const handleMenuItemClick = (path) => {
     setActiveItem(path);
-    toggleMobileMenu(); // Close the mobile menu after selecting an item
+    toggleMobileMenu();
   };
-
-  // update the header please
-  // update the header please
 
   return (
     <header className="th-header header-layout2">
@@ -37,35 +34,35 @@ const Header = () => {
           <div className="header-links">
             <ul>
               {/* remove the bounce */}
-              <li><i className="fa-solid fa-location-dot fa-bounce"></i>Staten Island, New York, US</li>
-              <li><i className="fa-brands fa-square-whatsapp fa-bounce"></i><a href="https://api.whatsapp.com/send/?phone=16142706554&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer">+1 (614) 270 6554</a></li>
-              <li><i className="fa-solid fa-envelope fa-bounce"></i><a href="mailto:info@superiorestimates.io">info@superiorestimates.net</a></li>
-              <li><i className="fa-solid fa-phone fa-bounce"></i><a href="tel:+1 (380) 268 9629" target="_blank" rel="noopener noreferrer">+1 (380) 268 9629</a></li>
+              <li className='d-flex align-items-center'><i className="fa-solid fa-location-dot" style={{fontSize:"12px"}}></i>Staten Island, New York, US</li>
+              <li className='d-flex align-items-center'><i className="fa-brands fa-whatsapp" style={{fontSize:"15px"}}></i><a href="https://api.whatsapp.com/send/?phone=16142706554&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer">+1 (614) 270 6554</a></li>
+              <li className='d-flex align-items-center'><i className="fa-solid fa-envelope" style={{fontSize:"15px"}}></i><a href="mailto:info@superiorestimates.io">info@superiorestimates.net</a></li>
+              <li className='d-flex align-items-center'><i className="fa-solid fa-phone" style={{fontSize:"12px"}}></i><a href="tel:+1 (380) 268 9629" target="_blank" rel="noopener noreferrer">+1 (380) 268 9629</a></li>
             </ul>
           </div>
           <div className="header-social">
             <span className="social-title">Follow Us On : </span>
             {/* <a href="https://www.facebook.com/superiorestimates" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a> */}
-            <a href="https://www.linkedin.com/company/superior-estimates-architectural-designs-seads/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
+            <a href="https://www.linkedin.com/company/superior-estimates-architectural-designs-seads/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in" style={{fontSize:"12px", marginTop:"4px"}}></i></a>
             {/* <a href="https://www.instagram.com/superiorestimates/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
             <a href="https://www.youtube.com/@superiorestimates" target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube"></i></a> */}
           </div>
         </div>
       </div>
       <div className="sticky-wrapper">
-        <div className="menu-area">
-          <div className="container">
+        <div className="menu-area" >
+          <div className="container" >
             <div className="row align-items-center justify-content-between">
               <div className="col-auto">
                 <div className="header-logo">
                   <Link className="icon-masking" to="/">
-                    <img src="assets/img/Logo/logo.png" alt="superiorestimates Logo" style={{ width: '170px', height: '55px' }} />
+                    <img src="assets/img/Logo/logo.png" alt="superiorestimates Logo" style={{ width: '130px', height: '45px' }} />
                   </Link>
                 </div>
               </div>
-              <div className="col">
+              <div className="col-auto">
                 <nav className={`main-menu d-none d-lg-inline-block ${isMobileMenuOpen ? 'open' : ''}`}>
-                  <ul className="menu-list">
+                  <ul className="menu-list " >
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/services">Services</Link></li>
                     <li><Link to="/hireus">Hire Us</Link></li>
@@ -77,13 +74,13 @@ const Header = () => {
                   </ul>
                 </nav>
               </div>
-              <div className="col-auto d-none d-lg-block">
+              {/* <div className="col-auto d-none d-lg-block">
                 <div className="header-button">
                   <a href="https://api.whatsapp.com/send/?phone=16142706554&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer" className="th-btn appointment-btn">
-                    Book an Appointment <i className="fa-brands fa-square-whatsapp fa-bounce"></i>
+                    Book an Appointment <i className="fa-brands fa-whatsapp"></i>
                   </a>
                 </div>
-              </div>
+              </div> */}
               <div className="col-auto d-lg-none">
                 <div className="header-button">
                   <button type="button" className="th-menu-toggle" onClick={toggleMobileMenu}>
